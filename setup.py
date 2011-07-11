dcdf43e96611db97534c4f975837a1dd0303cdfc
Daniel Miller <bonsaiviking@gmail.com>
2011-07-11 17:35:05 -0500

    Rename to NfSpy, reorder for distutils

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup( name='NfSpy',
+        version='1.0',
+        description='ID-spoofing NFS client',
+        author='Daniel Miller',
+        author_email='bonsaiviking@gmail.com',
+        url='https://github.com/bonsaiviking/nfspy',
+        packages=['nfspy'],
+        scripts=['nfspy/nfspy.py'],
+        )
