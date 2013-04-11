f541538fb5ccad1f7c7c727d82e995f9c07d35d3
Daniel Miller <daniel@bonsaiviking.com>
2013-04-11 16:19:52 -0500

    Update setup.py to install nfspysh

--- setup.py
+++ setup.py
@@ -2,6 +2,15 @@
 
 from distutils.core import setup
 
+scripts = ['scripts/nfspysh']
+try:
+    import fuse
+    scripts.append('scripts/nfspy')
+except ImportError:
+    print "python-fuse not detected, not installing nfspy."
+    print "You can still use nfspysh for a ftp-like interface"
+    pass
+
 setup( name='NfSpy',
         version='1.0',
         description='ID-spoofing NFS client',
@@ -9,6 +18,5 @@ setup( name='NfSpy',
         author_email='bonsaiviking@gmail.com',
         url='https://github.com/bonsaiviking/NfSpy',
         packages=['nfspy'],
-        requires=['fuse'],
-        scripts=['scripts/nfspy'],
+        scripts=scripts,
         )
